a3d78df95460f33ac9545df4e899dbbe38ef7e39
hiltonfarias <hilton.farias.da.silva@gmail.com>
2020-07-22 16:20:57 -0300

    Tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -16,3 +17,7 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
